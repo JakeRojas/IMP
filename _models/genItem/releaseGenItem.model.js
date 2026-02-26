a5a4fd2dcaff8eb16ec0639a94aca99ae11ae841
Jake Rojas <jakerojas2211@gmail.com>
2026-02-27 01:34:17 +0800

    38, fixing create button

--- _models/genItem/releaseGenItem.model.js
+++ _models/genItem/releaseGenItem.model.js
@@ -7,18 +7,19 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
     const attributes = {
-        releaseGenItemId:       { type: DataTypes.INTEGER,  primaryKey: true, autoIncrement: true },
-        roomId:                 { type: DataTypes.INTEGER,  allowNull: false },
-        genItemInventoryId:     { type: DataTypes.INTEGER,  allowNull:  false },
-        releasedBy:             { type: DataTypes.STRING,   allowNull:  false },
-        claimedBy:              { type: DataTypes.STRING,   allowNull:  false },
-        releaseItemQuantity:    { type: DataTypes.INTEGER,  allowNull:  false },
-        genItemType:            { type: DataTypes.ENUM(
-                                    'it', 'maintenance', 'unknownType'
-                                    ), 
-                                    allowNull: false 
-                                },
-        releasedAt:             { type: DataTypes.DATE,     allowNull:  false, defaultValue: DataTypes.NOW },
+        releaseGenItemId: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+        roomId: { type: DataTypes.INTEGER, allowNull: false },
+        genItemInventoryId: { type: DataTypes.INTEGER, allowNull: false },
+        releasedBy: { type: DataTypes.STRING, allowNull: false },
+        claimedBy: { type: DataTypes.STRING, allowNull: false },
+        releaseItemQuantity: { type: DataTypes.INTEGER, allowNull: false },
+        genItemType: {
+            type: DataTypes.ENUM(
+                'it', 'maintenance', 'unknownType'
+            ),
+            allowNull: false
+        },
+        releasedAt: { type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW },
     };
 
     const options = {
